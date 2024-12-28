978f0bb007fa7fbaef3f5223ec805feff65c7884
ArthurPhyto <arthur@swissharvest.com>
2024-12-28 18:01:16 +0100

    ttt

--- lib/youtube.ts
+++
@@ -1,8 +0,0 @@
-export function getYoutubeEmbedUrl(url: string): string {
-  try {
-    const videoId = new URL(url).searchParams.get('v');
-    return videoId ? `https://www.youtube.com/embed/${videoId}` : '';
-  } catch {
-    return '';
-  }
-}
